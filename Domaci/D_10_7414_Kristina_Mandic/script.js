de26fc30777673ff42d0c998641e1993adfa2ee7
kristinamandic <kristina.mandic98@gmail.com>
2021-12-24 19:54:31 +0100

    Deseti domaci

---
+++ Domaci/D_10_7414_Kristina_Mandic/script.js
@@ -0,0 +1,60 @@
+let dan = {
+    datum: "2021/12/24",
+    kisa: true,
+    sunce: false,
+    oblacno: true,
+    temp: [5, 12, 15, 15, 14, 10, 10],
+    //1. Zadatak 
+    tropski: function () {
+        let br = 0;
+        this.temp.forEach(t => {
+            if (t < 24) {
+                br++;
+            }
+        });
+        return br == 0;
+    },
+    // 2. Zadatak 
+    nepovoljan: function () {
+        let prethodni = this.temp[0];
+        let sljedeci = this.temp[0 + 1];
+        let br = 0;
+        for (let i = 0; i < this.temp.length; i++) {
+            prethodni = this.temp[i];
+            sljedeci = this.temp[i + 1];
+            if (prethodni - sljedeci > 8 || sljedeci - prethodni > 8) {
+                br++;
+            }
+        }
+        return br != 0;
+    },
+    // 3. Zadatak 
+    neuobicajen: function () {
+        let manjeOd5 = 0;
+        let viseOd25 = 0;
+        this.temp.forEach(t => {
+            if (t < -5) {
+                manjeOd5++;
+            }
+            else if (t > 25) {
+                viseOd25++;
+            }
+        });
+        if (this.kisa == true && manjeOd5 > 0) {
+            return true;
+        }
+        else if (this.oblacno == true && viseOd25 > 0) {
+            return true;
+        }
+        else if (this.kisa == true && this.oblacno == true && this.sunce == true) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
+
+console.log(dan.tropski());
+console.log(dan.nepovoljan());
+console.log(dan.neuobicajen());
