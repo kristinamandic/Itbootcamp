eadfc72d7650e5ec51d1f603ac489293189cd0a7
kristinamandic <kristina.mandic98@gmail.com>
2022-01-07 01:18:14 +0100

    nesto

---
+++ Domaci/SN_2_7414_Kristina_Mandic/KVIZ/script.js
@@ -0,0 +1,43 @@
+let pitanje1 = {
+    pitanje: "Koji je najveci hotelski lanac na svijetu?",
+    odgovori: ["InterContinental Hotels Group", "Marriott International", "Hyatt Hotels Corporation", "Jin Jiang"],
+    tacno: 0
+};
+
+let pitanje2 = {
+    pitanje: "Koliko hotela je u vlasnistvu najveceg svjetkog lanca hotela?",
+    odgovori: [3000, 4000, 5000, 6000, 7000, 8000],
+    tacno: 4
+};
+
+let pitanje3 = {
+    pitanje: "Koliko soba ima najveci hotelski lanac na svijetu?",
+    odgovori: ["2 500 000", "2 000 000", "1 500 000", "1 000 000", "500 000"],
+    tacno: 2
+};
+
+let pitanje4 = {
+    pitanje: "Koji je najveci hotel na svijetu?",
+    odgovori: ["Mandalay Bay", "First World Hotel & Plaza", "Caesars Palace", "Izmailovo Hotel"],
+    tacno: 1
+};
+
+let pitanje5 = {
+    pitanje: "Gdje se nalazi najveci hotel na svijetu?",
+    odgovori: ["Malezija", "Sjedinjene Americke Drzave", "Rusija", "Makao"],
+    tacno: 0
+};
+
+let pitanja = [pitanje1, pitanje2, pitanje3, pitanje5, pitanje5];
+
+// let random = 0;
+// let randomNiz = [];
+// let broj = 0;
+// for (let i = 1; i <= 10; i++) {
+//     random = Math.floor(Math.random() * 10) + 1;
+//     randomNiz.push(random);
+// }
+
+pitanja.forEach(p => {
+
+});
