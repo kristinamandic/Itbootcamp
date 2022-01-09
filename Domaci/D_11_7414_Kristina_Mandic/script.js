05773d4e0620306fae680d3fd21c4647465bb55e
kristinamandic <kristina.mandic98@gmail.com>
2022-01-09 11:43:55 +0100

    Jedanaesti domaci

--- Domaci/D_11_7414_Kristina_Mandic/script.js
+++ Domaci/D_11_7414_Kristina_Mandic/script.js
@@ -29,25 +29,65 @@ let knjiga4 = {
 let knjige = [knjiga1, knjiga2, knjiga3, knjiga4];
 
 
-let tabela = "";
-tabela += "<table>";
+let tabela = document.createElement("table");
+
 knjige.forEach((k, i) => {
+    // Redovi
+    let tr = document.createElement("tr");
     if (i % 2 == 0) {
-        if (k.procitana == true) {
-            tabela += `<tr style="background-color:purple"><td><img src="${k.putanja}" width="50%"></td><td><p style="color:blue">${k.naziv} - ${k.autor}</p></td></tr>`;
-        }
-        else {
-            tabela += `<tr style="background-color:purple"><td><img src="${k.putanja}" width="50%"></td><td><p style="color:gray">${k.naziv} - ${k.autor}</p></td></tr>`;
-        }
+        tr.style.backgroundColor = "purple";
     }
     else {
-        if (k.procitana == true) {
-            tabela += `<tr style="background-color:pink"><td><img src="${k.putanja}" width="50%"></td><td><p style="color:blue">${k.naziv} - ${k.autor}</p></td></tr>`;
-        }
-        else {
-            tabela += `<tr style="background-color:pink"><td><img src="${k.putanja}" width="50%"></td><td><p style="color:gray">${k.naziv} - ${k.autor}</p></td></tr>`;
-        }
+        tr.style.backgroundColor = "pink";
     }
+    tabela.appendChild(tr);
+
+    // Prva kolona
+    let td1 = document.createElement("td");
+    tr.appendChild(td1);
+    let slika = document.createElement("img");
+    slika.src = `${k.putanja}`;
+    slika.style.width = "50%";
+    td1.appendChild(slika);
+
+    // Druga kolona
+    let td2 = document.createElement("td");
+    tr.appendChild(td2);
+    let p = document.createElement("p");
+    p.textContent = `${k.naziv} - ${k.autor}`;
+    if (k.procitana == true) {
+        p.style.color = "blue";
+    }
+    else {
+        p.style.color = "gray";
+    }
+    td2.appendChild(p);
 });
-tabela += "</table>";
-document.body.innerHTML += tabela;
+
+document.body.appendChild(tabela);
+
+
+
+// Pravljenje tabele preko innerHTML - ovo sam prvo napravila, pa sam u razgovoru sa koligama shvatila da nije ptrebalo ovako, ali ne bih da brisem, pa ostavljam ovdje
+// let tabela = "";
+// tabela += "<table>";
+// knjige.forEach((k, i) => {
+//     if (i % 2 == 0) {
+//         if (k.procitana == true) {
+//             tabela += `<tr style="background-color:purple"><td><img src="${k.putanja}" width="50%"></td><td><p style="color:blue">${k.naziv} - ${k.autor}</p></td></tr>`;
+//         }
+//         else {
+//             tabela += `<tr style="background-color:purple"><td><img src="${k.putanja}" width="50%"></td><td><p style="color:gray">${k.naziv} - ${k.autor}</p></td></tr>`;
+//         }
+//     }
+//     else {
+//         if (k.procitana == true) {
+//             tabela += `<tr style="background-color:pink"><td><img src="${k.putanja}" width="50%"></td><td><p style="color:blue">${k.naziv} - ${k.autor}</p></td></tr>`;
+//         }
+//         else {
+//             tabela += `<tr style="background-color:pink"><td><img src="${k.putanja}" width="50%"></td><td><p style="color:gray">${k.naziv} - ${k.autor}</p></td></tr>`;
+//         }
+//     }
+// });
+// tabela += "</table>";
+// document.body.innerHTML += tabela;
---
+++ Domaci/SN_2_7414_Kristina_Mandic/KVIZ/script.js
@@ -0,0 +1,140 @@
+let pitanje1 = {
+    pitanje: "Koji je najveci hotelski lanac na svijetu?",
+    odgovori: ["InterContinental Hotels Group", "Marriott International", "Hyatt Hotels Corporation", "Jin Jiang"],
+    tacno: 0
+};
+
+let pitanje2 = {
+    pitanje: "Koliko hotela je u vlasnistvu najveceg svjetkog lanca hotela?",
+    odgovori: [3000, 4000, 5000, 6000, 7000, 8000],
+    tacno: 4
+};
+
+let pitanje3 = {
+    pitanje: "Koliko soba ima najveci hotelski lanac na svijetu?",
+    odgovori: ["2 500 000", "2 000 000", "1 500 000", "1 000 000", "500 000"],
+    tacno: 2
+};
+
+let pitanje4 = {
+    pitanje: "Koji je najveci hotel na svijetu?",
+    odgovori: ["Mandalay Bay", "First World Hotel & Plaza", "Caesars Palace", "Izmailovo Hotel"],
+    tacno: 1
+};
+
+let pitanje5 = {
+    pitanje: "Gdje se nalazi najveci hotel na svijetu?",
+    odgovori: ["Malezija", "Sjedinjene Americke Drzave", "Rusija", "Makao"],
+    tacno: 0
+};
+
+let pitanje6 = {
+    pitanje: "Gdje se nalazi najveci hotel na svijetu?",
+    odgovori: ["Malezija", "Sjedinjene Americke Drzave", "Rusija", "Makao"],
+    tacno: 0
+};
+
+let pitanje7 = {
+    pitanje: "Gdje se nalazi najveci hotel na svijetu?",
+    odgovori: ["Malezija", "Sjedinjene Americke Drzave", "Rusija", "Makao"],
+    tacno: 0
+};
+
+let pitanje8 = {
+    pitanje: "Gdje se nalazi najveci hotel na svijetu?",
+    odgovori: ["Malezija", "Sjedinjene Americke Drzave", "Rusija", "Makao"],
+    tacno: 0
+};
+
+let pitanje9 = {
+    pitanje: "Gdje se nalazi najveci hotel na svijetu?",
+    odgovori: ["Malezija", "Sjedinjene Americke Drzave", "Rusija", "Makao"],
+    tacno: 0
+};
+
+let pitanje10 = {
+    pitanje: "Gdje se nalazi najveci hotel na svijetu?",
+    odgovori: ["Malezija", "Sjedinjene Americke Drzave", "Rusija", "Makao"],
+    tacno: 0
+};
+
+let pitanje11 = {
+    pitanje: "Gdje se nalazi najveci hotel na svijetu?",
+    odgovori: ["Malezija", "Sjedinjene Americke Drzave", "Rusija", "Makao"],
+    tacno: 0
+};
+
+let pitanje12 = {
+    pitanje: "Gdje se nalazi najveci hotel na svijetu?",
+    odgovori: ["Malezija", "Sjedinjene Americke Drzave", "Rusija", "Makao"],
+    tacno: 0
+};
+
+let pitanja = [pitanje1, pitanje2, pitanje3, pitanje4, pitanje5, pitanje6, pitanje7, pitanje8, pitanje9, pitanje10, pitanje11, pitanje12];
+
+
+// Generisanje nasumicnih brojeva indeksa
+let randomBrojevi = arr => {
+    let max = arr.length;
+    let random = [];
+    for (let i = 0; i < max; i++) {
+        let broj = Math.floor(Math.random() * max);
+        if (random.indexOf(broj) == -1) {
+            random.push(broj);
+        }
+        else {
+            i--;
+        }
+    }
+    return random
+}
+console.log(randomBrojevi(pitanja));
+
+
+// Sortiranje pitanja pomoci nasumicnih brojeva indeksa
+let randomPitanja = arr => {
+    let brojevi = randomBrojevi(arr);
+    let noviNiz = []
+    for (let i = 0; i < arr.length; i++) {
+        noviNiz.push(arr[brojevi[i]]);
+    }
+    return noviNiz;
+}
+
+pitanja = randomPitanja(pitanja);
+
+
+// Podjela niza na nizove od po 5 objekata
+let a = [];
+let b = [];
+let c = [];
+for (let i = 0; i < pitanja.length; i++) {
+    if (i < 5) {
+        a.push(pitanja[i]);
+    }
+    else if (i < 10) {
+        b.push(pitanja[i]);
+    }
+    else {
+        c.push(pitanja[i]);
+    }
+}
+
+/////////////////////////////////
+let h2;
+let fieldset;
+let radio;
+
+pitanja.forEach((p, i) => {
+    i++;
+    fieldset = document.createElement("fieldset");
+    document.body.appendChild(fieldset);
+    h2 = document.createElement("h4");
+    h2.innerHTML = `${i}. ${p.pitanje}`;
+    fieldset.appendChild(h2)
+    p.odgovori.forEach(o => {
+        radio = document.createElement("input");
+        radio.innerHTML = `${o}`;
+        fieldset.appendChild(radio)
+    });
+});
