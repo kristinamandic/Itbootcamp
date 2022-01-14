9e76ea5a17b66a6ae23450a0db062e80ee826230
kristinamandic <kristina.mandic98@gmail.com>
2022-01-14 17:13:52 +0100

    21_KLASE VJEZBANJA

--- 21_KLASE/FILM/film.js
+++ 21_KLASE/FILM/film.js
@@ -5,13 +5,18 @@ class Film {
         this.godinaIzdanja = gi;
         this.ocjene = o;
     }
+
+    // METOD ZA STAMPAJ
     stampaj() {
         console.log(this.naslov);
     }
-    // DODAVANJE JEDNE OCJENE 
+
+    // METOD ZA DODAVANJE JEDNE OCJENE 
     dodajOcjenu(novaOcjena) {
         this._ocjene.push(novaOcjena);
     }
+
+    // METOD ZA PROSJEK
     prosjek() {
         let zbir = 0;
         this.ocjene.forEach(o => {
@@ -20,6 +25,7 @@ class Film {
         return zbir / this.ocjene.length;
     }
 
+    // SETER I GETER ZA NASLOV
     set naslov(n) {
         let n1 = n.trim();
         this._naslov = n1;
@@ -28,6 +34,7 @@ class Film {
         return this._naslov;
     }
 
+    // SETER I GETER ZA REZISER
     set reziser(r) {
         let r1 = r.trim();
         this._reziser = r1;
@@ -36,6 +43,7 @@ class Film {
         return this._reziser;
     }
 
+    // SETER I GETER ZA GODINU IZDANJA
     set godinaIzdanja(gi) {
         if (gi > 1800) {
             this._godinaIzdranja = gi;
@@ -48,6 +56,7 @@ class Film {
         return this._godinaIzdranja;
     }
 
+    // SETER I GETER ZA OCJENE
     set ocjene(o) {
         this._ocjene = o;
     }
@@ -56,4 +65,112 @@ class Film {
     }
 }
 
-export default Film;
+let vijekFilmova = (arr, x) => {
+    let pocetak = x * 100 - 99;
+    let kraj = x * 100;
+    arr.forEach(f => {
+        if (f.godinaIzdanja >= pocetak && f.godinaIzdanja <= kraj) {
+            console.log(f.naslov);
+        }
+    });
+    // MOJ NACIN - RADI ALI NIJE DOBAR
+    // arr.forEach(f => {
+    //     if (x == 19) {
+    //         if (f.godinaIzdanja >= 1800 && f.godinaIzdanja < 1900) {
+    //             console.log(f);
+    //         }
+    //     }
+    //     else if (x == 20) {
+    //         if (f.godinaIzdanja >= 1900 && f.godinaIzdanja < 2000) {
+    //             console.log(f);
+    //         }
+    //     }
+    //     else if (x == 21) {
+    //         if (f.godinaIzdanja >= 2000 && f.godinaIzdanja < 2100) {
+    //             console.log(f);
+    //         }
+    //     }
+    //     else {
+    //         console.log("Unjeli ste pogresan vijek");
+    //     }
+    // });
+};
+
+let prosjecnaOcjena = arr => {
+    let broj = 0;
+    let zbir = 0;
+    arr.forEach(f => {
+        f.ocjene.forEach(o => {
+            broj++;
+            zbir += o;
+        });
+    });
+    return zbir / broj;
+};
+
+let najboljeOcjenjeni = arr => {
+    let max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (max.prosjek() < arr[i].prosjek()) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+let osrednjiFilm = arr => {
+    let globalniProsjek = prosjecnaOcjena(arr);
+    let filmNajbliziProsjeku = arr[0]; // pretpostavka da je prvi film najblizi prosjeku
+    let najmanjaRazlikaOdProsjeka = Math.abs(globalniProsjek - filmNajbliziProsjeku.prosjek());
+    arr.forEach(f => {
+        let prosjecnaOcjenaFilma = f.prosjek();
+        if (Math.abs(globalniProsjek - prosjecnaOcjenaFilma) < najmanjaRazlikaOdProsjeka) {
+            najmanjaRazlikaOdProsjeka = Math.abs(globalniProsjek - prosjecnaOcjenaFilma);
+            filmNajbliziProsjeku = f;
+        }
+    });
+    return filmNajbliziProsjeku;
+    // DOBRO JE, ALI SAM ISLA PJESKE
+    // let film = arr[0];
+    // let minRazlika = prosjecnaOcjena(arr) - arr[0].prosjek();
+    // if (minRazlika < 0) {
+    //     minRazlika = -minRazlika;
+    // }
+    // arr.forEach(f => {
+    //     let sadaRazlika = prosjecnaOcjena(arr) - f.prosjek();
+    //     if (sadaRazlika < 0) {
+    //         sadaRazlika = -sadaRazlika;
+    //     }
+    //     if (minRazlika > sadaRazlika) {
+    //         minRazlika = sadaRazlika;
+    //         film = f;
+    //     }
+    // });
+    // return film;
+};
+
+let najmanjaOcjenaNajboljeg = arr => {
+    let najbolji = najboljeOcjenjeni(arr);
+    let min = najbolji.ocjene[0];
+    najbolji.ocjene.forEach(o => {
+        if (min > o) {
+            min = o;
+        }
+    });
+    console.log(min);
+};
+
+let najmanjaOcjena = arr => {
+    let min = arr[0].ocjene[0];
+    arr.forEach(f => {
+        f.ocjene.forEach(o => {
+            if (min > o) {
+                min = o;
+            }
+        });
+    });
+    return min;
+};
+
+
+export { Film, vijekFilmova, prosjecnaOcjena, najboljeOcjenjeni, osrednjiFilm, najmanjaOcjenaNajboljeg, najmanjaOcjena };
--- 21_KLASE/FILM/main.js
+++ 21_KLASE/FILM/main.js
@@ -1,4 +1,4 @@
-import Film from "./film.js";
+import { Film, vijekFilmova, prosjecnaOcjena, najboljeOcjenjeni, osrednjiFilm, najmanjaOcjenaNajboljeg, najmanjaOcjena } from "./film.js";
 
 
 let film1 = new Film("The Silence of the Lambs", "Johnathan Demme", 1991, [8.8, 8.9, 8.2, 9.7, 9.6]);
@@ -18,8 +18,6 @@ console.log(filmovi);
 film1.dodajOcjenu(10);
 console.log(film1);
 
-console.log(film3.prosjek());
-
 // Prikaz svih filmova u tabeli
 let tabelica =
     `<table border="1">
@@ -28,6 +26,7 @@ let tabelica =
             <th>Reziser</th>
             <th>Godina izdanja</th>
             <th>Ocjene</th>
+            <th>Prosjecna</th>
         </tr>
 `;
 
@@ -38,9 +37,110 @@ filmovi.forEach(f => {
             <td>${f.reziser}</td>
             <td>${f.godinaIzdanja}</td>
             <td>${f.ocjene}</td>
+            <td>${f.prosjek()}</td>
         </tr>`;
 });
 
 tabelica += `</table>`;
 
 document.body.innerHTML = tabelica;
+
+
+// Prosjek, vijekFilmova, prosjecnaOcjena
+
+console.log(film1.prosjek());
+console.log(film2.prosjek());
+console.log(film3.prosjek());
+
+vijekFilmova(filmovi, 21);
+
+console.log(prosjecnaOcjena(filmovi));
+
+console.log(najboljeOcjenjeni(filmovi));
+
+console.log(osrednjiFilm(filmovi));
+
+najmanjaOcjenaNajboljeg(filmovi);
+
+console.log(najmanjaOcjena(filmovi));
+
+
+//Napisati funkciju najcescaOcena kojoj se prosleđuje niz ocena, a ona vraća ocenu koju su filmovi najčešće dobijali. 
+
+// Prvo pravimo niz svih ocjena
+let sveOcjene = arr => {
+    // MOJ NACIN
+    // let sveOcjene = [];
+    // arr.forEach(f => {
+    //     f.ocjene.forEach(o => {
+    //         sveOcjene.push(o);
+    //     });
+    // });
+    // return sveOcjene;
+
+    // CONCAT moze odjednom da se vrsi, ili pojedinacno:
+    // Odjednom bi glasilo: sveOcjene = sveOcjene.concat(film1, film2, film3)
+    // Pojedinacno cemo ovako:
+    let arrSveOcjene = [];
+    arr.forEach(f => {
+        arrSveOcjene = arrSveOcjene.concat(f.ocjene);
+    });
+    return arrSveOcjene;
+};
+
+// Niz koji prosljedjujemo je niz svih ocjena
+let najcescaOcjena = arr => {
+    let pretpostavkaElem = arr[0];
+    let pretpostavkaElemPojavljivanja = 1; // sigurno se jednom pojavio
+
+    for (let i = 0; i < arr.length; i++) {
+        let tekuci = arr[i];
+        let tekuciBrPojavljivanja = 0;
+
+        for (let j = 0; j < arr.length; j++) {
+            if (tekuci == arr[j]) {
+                tekuciBrPojavljivanja++;
+            }
+        }
+
+        if (pretpostavkaElemPojavljivanja < tekuciBrPojavljivanja) {
+            pretpostavkaElemPojavljivanja = tekuciBrPojavljivanja;
+            pretpostavkaElem = tekuci;
+        }
+    }
+    return pretpostavkaElem;
+};
+
+console.log(sveOcjene(filmovi));
+console.log(najcescaOcjena(sveOcjene(filmovi)));
+
+
+// Napraviti funkciju iznadOcene kojoj se prosleđuje ocena i niz filmova, a ona vraća niz onih filmova koji su bolje ocenjeni (veća im je prosečna ocena) od prosleđene ocene.
+
+let iznadOcjene = (arr, x) => {
+    let nizFilmova = [];
+    arr.forEach(f => {
+        if (f.prosjek() > x) {
+            nizFilmova.push(f);
+        }
+    });
+    return nizFilmova;
+};
+
+console.log(iznadOcjene(filmovi, 8.9));
+
+
+// Napisati funkciju iznadOceneNoviji kojoj se prosleđuje ocena i niz filmova  a koja treba na ekranu da ispiše sve podatke o najnovijem filmu koji zadovoljava prosleđenu ocenu
+
+let iznadOceneNoviji = (arr, x) => {
+    let iznadOcjeneFilmovi = iznadOcjene(arr, x);
+    let max = iznadOcjeneFilmovi[0];
+    iznadOcjeneFilmovi.forEach(f => {
+        if (f.godinaIzdanja > max.godinaIzdanja) {
+            max = f;
+        }
+    });
+    console.log(max);
+}
+
+iznadOceneNoviji(filmovi, 8);
