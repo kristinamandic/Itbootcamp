177d58242ddc9421be5a63bd75cd554d133f5fd4
kristinamandic <kristina.mandic98@gmail.com>
2022-01-27 21:46:23 +0100

    28 Baze Podataka

--- 28_BAZE_PODATAKA/vjezbanja_tasks.js
+++ 28_BAZE_PODATAKA/vjezbanja_tasks.js
@@ -7,7 +7,7 @@ db.collection("tasks")
             let allDocs = snapshot.docs;
             allDocs.forEach(doc => {
                 let obj = doc.data();
-                console.log(obj.title, obj.title, obj.priority);
+                console.log(obj.title, obj.description, obj.priority);
             });
         }
         else {
@@ -29,7 +29,7 @@ db.collection("tasks")
             let allDocs = snapshot.docs;
             allDocs.forEach(doc => {
                 let obj = doc.data();
-                console.log(obj.title, obj.title, obj.priority);
+                console.log(obj.title, obj.description, obj.priority);
             });
         }
         else {
@@ -44,17 +44,18 @@ db.collection("tasks")
 // b) Treba da se izvrše u tekućoj godini
 let datum = new Date();
 let godina = datum.getFullYear();
-let krajGodine = new Date(`${godina}-12-31 24:00:00`);
-let kraj = firebase.firestore.Timestamp.fromDate(krajGodine);
+let krajGodine = new Date(`${godina}-12-31 23:59:59`);
+
+let krajGodineMls = firebase.firestore.Timestamp.fromDate(krajGodine);
 db.collection("tasks")
-    .where("due_date", "<=", kraj)
+    .where("due_date", "<=", krajGodineMls)
     .get()
     .then(snapshot => {
         if (!snapshot.empty) {
             let allDocs = snapshot.docs;
             allDocs.forEach(doc => {
                 let obj = doc.data();
-                console.log(obj.title, obj.title, obj.priority);
+                console.log(obj.title, obj.description, obj.priority);
             });
         }
         else {
@@ -67,3 +68,48 @@ db.collection("tasks")
 
 
 // c) Su završeni
+let dan = datum.getDate();
+let mjesec = datum.getMonth() + 1;
+let sat = datum.getHours();
+let minut = datum.getMinutes();
+let sekunde = datum.getSeconds();
+let datumDanas = new Date(`${godina}-${mjesec}-${dan} ${sat}:${minut}:${sekunde}`);
+
+let datumDanasMls = firebase.firestore.Timestamp.fromDate(datumDanas);
+db.collection("tasks")
+    .where("due_date", "<", datumDanasMls)
+    .get()
+    .then(snapshot => {
+        if (!snapshot.empty) {
+            snapshot.docs.forEach(doc => {
+                let obj = doc.data();
+                console.log(obj.title, obj.description, obj.priority);
+            });
+        }
+        else {
+            console.log("Nema dokumenta koji odgovara zahtjevu");
+        }
+    })
+    .catch(err => {
+        console.log(`Doslo je do greske: ${err}`);
+    })
+
+
+// d) Tek treba da počnu
+db.collection("tasks")
+    .where("start_date", ">", datumDanasMls)
+    .get()
+    .then(snapshot => {
+        if (!snapshot.empty) {
+            snapshot.docs.forEach(doc => {
+                let obj = doc.data();
+                console.log(obj.title, obj.description, obj.priority);
+            });
+        }
+        else {
+            console.log("Nema dokumenta koji odgovara zahtjevu");
+        }
+    })
+    .catch(err => {
+        console.log(`Doslo je do greske: ${err}`);
+    })
