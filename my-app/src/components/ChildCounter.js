ff33bcfeca528d35c3113d23ce88fd41debdd14e
kristinamandic <kristina.mandic98@gmail.com>
2022-02-10 20:00:03 +0100

    Dopuna

--- my-app/src/components/ChildCounter.js
+++ my-app/src/components/ChildCounter.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 
 class ChildCounter extends Component {
-    state = {
-        // value: this.props.value
-    }
+    // state = {
+    //     value: this.props.value
+    // }
+
+    // handleIncrement = () => {
+    //     this.setState({ value: this.state.value + 1 })
+    // }
+
     render() {
         return (
-            <div>#{this.props.value}
+            <div style={{ margin: 15 }}>
+                #{this.props.counter.value}
+                {/* #{this.state.value} */}
                 {/* <p>Ovo je iz state-a : {this.state.value}</p> */}
+                <button onClick={() => this.props.onIncrement(this.props.counter.id)}>
+                    +
+                </button>
+                <button onClick={() => this.props.onDelete(this.props.counter.id)}>Delete</button>
             </div>
         );
     }
--- my-app/src/components/Counters.js
+++ my-app/src/components/Counters.js
@@ -5,16 +5,62 @@ class Counters extends Component {
     state = {
         counters: [
             { id: 0, value: 0 },
-            { id: 2, value: 1 },
-            { id: 3, value: 2 },
-            { id: 4, value: 3 }
+            { id: 1, value: 1 },
+            { id: 2, value: 2 },
+            { id: 3, value: 3 }
         ]
     }
+
+    hendleIncrement = (id) => {
+        // Spread operator
+        // const counters = [...this.state.counters];
+        // const counters = {...this.state.counters[0], name: "Child"};
+
+        this.setState({
+            counters: this.state.counters.map((item) => {
+                return item.id === id ? { ...item, value: item.value + 1 } : item;
+            })
+        })
+    }
+
+    handleDelete = (id) => {
+        // Moja neka ideja, ne radi
+        // for (let i = 0; i <= this.state.counters.length; i++) {
+        //     return i === id ? this.state.counters.splice(i, 1) : this.state.counters[i];
+        // }
+
+        this.setState({
+            counters: this.state.counters.filter(counter => counter.id !== id)
+        });
+
+        // Marijin nacin - dugacko
+        // this.setState((previousState) => {
+        //     return {
+        //         ...previousState,
+        //         counters: previousState.counters.filter(item => {
+        //             item.id !== id
+        //         })
+        //     }
+        // })
+    }
+
     render() {
         return (
             <div>
                 {this.state.counters.map((counter) => (
-                    <ChildCounter key={counter.id} value={counter.value} />
+                    // <ChildCounter
+                    //     key={counter.id}
+                    //     id={counter.id}
+                    //     value={counter.value}
+                    //     counter={counter}
+                    //     onIncrement={this.hendleIncrement}
+                    // />
+                    <ChildCounter
+                        key={counter.id}
+                        counter={counter}
+                        onIncrement={this.hendleIncrement}
+                        onDelete={this.handleDelete}
+                    />
                 ))}
             </div>
         );
